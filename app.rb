e30aff4d24bf37f4aeb746cc80f52684d2d61c92
enrique & paul <student@epicodus.com>
2015-06-09 16:31:15 -0700

    pass all specs and app working as expected

--- app.rb
+++ app.rb
@@ -19,5 +19,37 @@ post("/lists") do
   name = params.fetch("name")
   list = List.new({:name => name, :id => nil})
   list.save()
-  erb(:list_success)
+  erb(:success)
+end
+
+get('/lists') do
+  @lists = List.all
+  erb(:lists)
+end
+
+get('/lists/:id') do
+  @list = List.find(params.fetch("id").to_i())
+  @tasks = Task.all
+  erb(:list)
+end
+
+post('/lists') do
+  name = params.fetch("name")
+  new_list = List.new({:name => name, :id => nil})
+  new_list.save()
+  erb(:success)
+end
+
+get('/lists/:id/tasks/new') do
+  @list = List.find(params.fetch("id").to_i())
+  erb(:task_form)
+end
+
+post("/tasks") do
+  description = params.fetch("description")
+  list_id = params.fetch("list_id").to_i()
+  @list = List.find(list_id)
+  @task = Task.new({:description => description, :list_id => list_id})
+  @task.save()
+  erb(:success)
 end
